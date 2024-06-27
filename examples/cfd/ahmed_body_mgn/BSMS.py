8fa9c36c7f54ddc940aae61e6189f4f677be61d4
Yadi Cao <yadicao95@ucla.edu>
2024-06-27 01:11:25 -0700

    refine readability of BSMS and ops; ready for Zijie to modify to modulus style

--- examples/cfd/ahmed_body_mgn/BSMS.py
+++ examples/cfd/ahmed_body_mgn/BSMS.py
@@ -1,69 +1,107 @@
 import torch.nn as nn
+import torch
 from ops import GMP, Unpool, WeightedEdgeConv
+# NOTE for zijie
+# you may need to translate the PYG code into modulus style
 
 
 class BSGMP(nn.Module):
-    def __init__(self, l_n, ld, hidden_layer, pos_dim, MP_model=GMP):
+    """Bistride Graph Message Passing (BSGMP) network for hierarchical graph processing."""
+
+    def __init__(self, unet_depth, latent_dim, hidden_layer, pos_dim):
+        """
+        Initialize the BSGMP network.
+
+        Parameters
+        ----------
+        unet_depth : int
+            Number of unet depth in the network.
+            NOTE this excepts the top level
+        latent_dim : int
+            Latent dimension for the graph nodes and edges.
+        hidden_layer : int
+            Number of hidden layers in the MLPs.
+        pos_dim : int
+            Dimension of the physical position (in Euclidean space).
+        """
         super(BSGMP, self).__init__()
-        self.bottom_gmp = MP_model(ld, hidden_layer, pos_dim)
+        self.bottom_gmp = GMP(latent_dim, hidden_layer, pos_dim)
         self.down_gmps = nn.ModuleList()
         self.up_gmps = nn.ModuleList()
         self.unpools = nn.ModuleList()
-        self.l_n = l_n
+        self.unet_depth = unet_depth
         self.edge_conv = WeightedEdgeConv()
-        for _ in range(self.l_n):
-            self.down_gmps.append(MP_model(ld, hidden_layer, pos_dim))
-            self.up_gmps.append(MP_model(ld, hidden_layer, pos_dim))
+        for _ in range(self.unet_depth):
+            self.down_gmps.append(GMP(latent_dim, hidden_layer, pos_dim))
+            self.up_gmps.append(GMP(latent_dim, hidden_layer, pos_dim))
             self.unpools.append(Unpool())
 
-    def forward(self, h, m_ids, m_gs, pos, weights=None):
-        # h is in shape of (T), N, F
-        # if edge_set_num>1, then m_g is in shape: Level,(Set),2,Edges, the 0th Set is main/material graph
-        # pos is in (T),N,D
-        down_outs = []
-        down_ps = []
-        cts = []
-        hs = []
-        w = pos.new_ones((pos.shape[-2], 1)) if weights is None else weights
-        # down pass
-        for i in range(self.l_n):
+    def forward(self, h, m_ids, m_gs, pos):
+        """
+        Forward pass for the BSGMP network.
+
+        Parameters
+        ----------
+        h : torch.Tensor
+            Node features of shape [B, N, F] or [N, F].
+        m_ids : list of torch.Tensor
+            Indices for pooling/unpooling nodes at each level.
+        m_gs : list of torch.Tensor
+            Graph connectivity (edges) at each level.
+        pos : torch.Tensor
+            Node positional information of shape [B, N, D] or [N, D].
+
+        Returns
+        -------
+        torch.Tensor
+            Updated node features.
+        """
+        # Shape: h is in (B, N, F) or (N, F)
+        # m_gs is in shape: Level,(Set),2,Edges, where 0th Set is main/material graph
+        # pos is in (B, N, D) or (N, D)
+
+        down_outs = []  # to store output features at each level during down pass
+        down_ps = []  # to store positional information at each level during down pass
+        cts = []  # to store edge weights for convolution at each level
+
+        w = pos.new_ones((pos.shape[-2], 1))  # Initialize weights
+
+        # Down pass
+        for i in range(self.unet_depth):
             h = self.down_gmps[i](h, m_gs[i], pos)
-            # record the infor before aggregation
             down_outs.append(h)
             down_ps.append(pos)
-            # aggregate then pooling
-            # cal edge_weights
-            m_gs[i]
-            ew, w = self.edge_conv.cal_ew(w, tmp_g)
-            h = self.edge_conv(h, tmp_g, ew)
-            pos = self.edge_conv(pos, tmp_g, ew)
+
+            # Calculate edge weights
+            ew, w = self.edge_conv.cal_ew(w, m_gs[i])
+            h = self.edge_conv(h, m_gs[i], ew)
+            pos = self.edge_conv(pos, m_gs[i], ew)
             cts.append(ew)
-            # pooling
+
+            # Pooling
             if len(h.shape) == 3:
                 h = h[:, m_ids[i]]
             elif len(h.shape) == 2:
                 h = h[m_ids[i]]
+
             if len(pos.shape) == 3:
                 pos = pos[:, m_ids[i]]
             elif len(pos.shape) == 2:
                 pos = pos[m_ids[i]]
+
             w = w[m_ids[i]]
-        # bottom pass
-        h = self.bottom_gmp(h, m_gs[self.l_n], pos)
-        # up pass
-        for i in range(self.l_n):
-            up_idx = self.l_n - i - 1
-            g, idx = m_gs[up_idx], m_ids[up_idx]
-            h = self.unpools[i](h, down_outs[up_idx].shape[-2], idx)
-            tmp_g = g
-            h = self.edge_conv(h, tmp_g, cts[up_idx], aggragating=False)
-            h = self.up_gmps[i](h, g, down_ps[up_idx])
-            h = h.add(down_outs[up_idx])
+
+        # Bottom pass
+        h = self.bottom_gmp(h, m_gs[self.unet_depth], pos)
+
+        # Up pass
+        for i in range(self.unet_depth):
+            depth_idx = self.unet_depth - i - 1
+            g, idx = m_gs[depth_idx], m_ids[depth_idx]
+            h = self.unpools[i](h, down_outs[depth_idx].shape[-2], idx)
+            # aggregate is False as we are returning the information to previous out degrees.
+            h = self.edge_conv(h, g, cts[depth_idx], aggragating=False)
+            h = self.up_gmps[i](h, g, down_ps[depth_idx])
+            h = h.add(down_outs[depth_idx])
 
         return h
-
-
-
-
-
-
